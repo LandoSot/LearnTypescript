6f860b88821caa3609f03cae8d3745f9cebba69d
Orlando Sotelo <ing.soteloc@gmail.com>
2022-07-18 02:20:28 -0500

    Create the Navigation
    Create the whole initial navigation of the app according the
    course, using Stack and BottomTab.
    Use onlye one commit due this is already known information.

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { NativeStackHeaderProps } from '@react-navigation/native-stack'
+import { Text, View } from 'react-native'
+
+export default function HomeSrcreen({ navigation }: NativeStackHeaderProps) {
+  return (
+    <View>
+      <Text>I am home screen</Text>
+    </View>
+  )
+}
