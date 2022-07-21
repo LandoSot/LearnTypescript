2cb14392e4d0a69a8a1f59a9b6af699d1b1b5433
Orlando Sotelo <danugfe@gmail.com>
2022-07-20 23:18:44 -0500

    Create interface type and data json
    Create a json file with example data to work with.
    Create a interface to define the types of the data json
    file, to set it as type at using.
    Include a FlatList to render the data json file in the
    HomeScreen with the interface type created.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,22 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack'
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
+import { Workout } from '../JSON/dataTypes'
+import data from '../JSON/data.json'
 
 export default function HomeSrcreen({ navigation }: NativeStackHeaderProps) {
+  const renderItem = ({ item }: { item: Workout }) => {
+    return (
+      <Text>{item.name} - {item.difficulty}</Text>
+    )
+  }
+
   return (
     <View>
-      <Text>I am home screen</Text>
+      <FlatList
+        data={data as Workout[]}
+        keyExtractor={item => item.slug}
+        renderItem={renderItem}
+      />
     </View>
   )
 }
