04ceb17c4fd7608532d0bb8dfb608d2d7695ebba
Orlando Sotelo <danugfe@gmail.com>
2022-07-22 21:39:44 -0500

    Font Loading
    Create a custom hook called useCachedResources to load
    data and resources at called.
    Add fonts files to the Assest folder and loaded them
    with the useCachedResources at the app open.
    Create a custom component for texts with the
    loaded fonts.
    Refactor the WorkoutItem to use the custom component
    with the loaded fonts and passing the styles.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar'
 import RootNav from './src/Navigation/Stack'
+import useCachedResources from './src/Hooks/useCachedResources'
 
 export default function App() {
+  const isLoaded = useCachedResources()
+
   return (
-    <>
-      <StatusBar style='auto' />
-      <RootNav />
-    </>
+    isLoaded ?
+      <>
+        <StatusBar style='auto' />
+        <RootNav />
+      </> : null
   )
 }
