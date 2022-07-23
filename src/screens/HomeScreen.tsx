d3c20f98a1dd851b816a4bf147829f73e48acd36
Orlando Sotelo <danugfe@gmail.com>
2022-07-22 21:54:27 -0500

    Fix capital letter in the folder and file names

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 import { FlatList, StyleSheet, Text, View } from 'react-native'
-import { Workout } from '../JSON/dataTypes'
-import data from '../JSON/data.json'
+import { Workout } from '../JSON/DataTypes'
+import data from '../JSON/Data.json'
 import WorkoutItem from '../Components/WorkoutItem'
 
 export default function HomeSrcreen({ navigation }: NativeStackHeaderProps) {
